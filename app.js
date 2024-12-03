6a8e532d21385c72ed0f18afdfc6b8206762f697
Jordan King <jordan.king@bensonsforbeds.co.uk>
2024-12-03 11:43:17 +0000

    Testing console logs

--- app.js
+++ app.js
@@ -78,6 +78,10 @@ function buildWeatherCard(name, current, timezone_offset){
     const sunriseTime = getTime(sunrise, timezone_offset)
     const sunsetTime = getTime(sunset, timezone_offset)
 
+    console.log()
+    console.log()
+    console.log()
+
     const sunriseContainer = document.createElement('div')
     sunriseContainer.innerText = getTime(sunrise, timezone_offset)
 
